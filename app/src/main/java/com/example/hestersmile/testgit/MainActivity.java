6d2543ea8c6dc91879d8208afbe002d02e3053b4
hestersy <838503907@qq.com>
2016-08-06 16:52:43 +0800

    hahahhahahah

--- app/src/main/java/com/example/hestersmile/testgit/MainActivity.java
+++ app/src/main/java/com/example/hestersmile/testgit/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("hahah");
+
+
+        /**
+         *
+         *
+         *
+         *
+         */
     }
 }
